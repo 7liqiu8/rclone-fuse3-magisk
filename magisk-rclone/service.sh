1b3ff23eb52be5daecb7a3db5db05155d7ca1cd8
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-09 12:15:30 +0800

    Update service.sh

--- magisk-rclone/service.sh
+++ magisk-rclone/service.sh
@@ -5,7 +5,7 @@ L(){
   log -t Magisk "[rclone] $1"
 }
 
-L "service script started:"  
+L "service script started:"
 
 [ "${MODPATH}"x = ""x ] && MODPATH="${0%/*}"
 L "load env: $MODPATH/env"
@@ -13,14 +13,14 @@ set -a && . "$MODPATH/env" && set +a
 
 sed -i 's/^description=\(.\{1,4\}| \)\?/description=/' "$RCLONEPROP"
 
-# Wait for the system to boot completely
 COUNT=0
-until { [ "$(getprop sys.boot_completed)" = "1" ] && [ "$(getprop init.svc.bootanim)" = "stopped" ] && [ -e "/sdcard" ]; } || [ $((COUNT++)) -ge 20 ]; do 
-  sleep 10;
+until { [ "$(getprop sys.boot_completed)" = "1" ] && [ "$(getprop init.svc.bootanim)" = "stopped" ] && [ -e "/sdcard" ]; } || [ $((COUNT++)) -ge 20 ]; do
+  sleep 10
 done
 L "system is ready after ${COUNT}. Starting the mounting process."
 
 /vendor/bin/rclone listremotes | sed 's/:$//' | while read -r remote; do
+  [ -n "$remote" ] || continue
   L "mount $remote => /mnt/rclone-$remote => /sdcard/$remote"
   /vendor/bin/rclone-mount "$remote" --daemon
 done
@@ -29,7 +29,7 @@ L "all remotes mounted successfully."
 
 sed -i 's/^description=\(.\{1,4\}| \)\?/description=🚀| /' "$RCLONEPROP"
 
-# rclone sync
+rm -f "$RCLONESYNC_PID"
 nice -n 19 ionice -c3 "$MODPATH/sync.service.sh" &
 echo $! > "$RCLONESYNC_PID"
 L "sync.service.sh started, PID: $(cat "$RCLONESYNC_PID")"
